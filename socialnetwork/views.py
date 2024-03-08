ac298d005593e3e03833600516b70b76a7f75aea
Mariana <marianar@andrew.cmu.edu>
2024-03-07 21:41:44 -0500

    adding basic html

--- socialnetwork/views.py
+++ socialnetwork/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'socialnetwork/home.html')
+    return render(request, 'socialnetwork/home.html', {'range': range(18) })
 
 def login(request):
     return render(request, 'socialnetwork/login.html')
